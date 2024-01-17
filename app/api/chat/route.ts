9419f1ca2f531089f11ceaeb47f62dd137d28cb0
Arslan Akishev <arswebsiterr@gmail.com>
2024-01-17 19:21:16 +0600

    title fix

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -21,12 +21,13 @@ export async function POST(req: Request) {
 
       // Extracting the last message as the question
       const lastMessage = messages[messages.length - 1];
+      console.log('last message = ', lastMessage)
       const question = lastMessage?.content;
       //@ts-ignore
-      const chatHistory = messages.slice(0, -1).map(message => message.content).join('\n');
+      const chatHistory = messages.slice(0).map(message => message.content).join('\n');
       console.log('hist:', chatHistory)
       const streamingResponse = await callChain({ question, chatHistory });
-        const title = question.substring(0, 100);
+        const title = messages[0]?.content.substring(0, 100);
         const id = json.id ?? nanoid();
         const createdAt = Date.now();
         const path = `/chat/${id}`;
